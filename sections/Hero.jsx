b55891fd8cd57444d123196f4f8de074770fa957
shanni6 <240686886@qq.com>
2022-12-02 11:42:45 +0800

    about

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -6,7 +6,12 @@ import { slideIn, staggerContainer,textVariant } from "../utils/motion";
 const Hero = () => (
   <section className={`$
   {styles.yPaddings} sm:pl-16 pl-6`}>
-    <motion.div/>
+    <motion.div
+    variants={staggerContainer}
+    initial="hidden"
+    whileInView="show"
+    viewport= {{once:false, amount :0.25}}
+    />
   </section>
 );
 
