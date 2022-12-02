5df9f1a47191ee7e54e16f08f9cd2c8a3e8370f3
shanni6 <240686886@qq.com>
2022-12-02 09:52:35 +0800

    navbar

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,8 +1,12 @@
 'use client';
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { slideIn, staggerContainer,textVariant } from "../utils/motion";
 
 const Hero = () => (
-  <section>
-    Hero section
+  <section className={`$
+  {styles.yPaddings} sm:pl-16 pl-6`}>
+    <motion.div/>
   </section>
 );
 
